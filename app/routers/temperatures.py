fe658e2f8a6507ab8dbb96b728df780d5ffd81e4
Ihor <il.cegelnik@gmail.com>
2025-10-01 16:49:44 +0300

    Added get_temperatures & load_temperatures to routers

--- app/routers/temperatures.py
+++ app/routers/temperatures.py
@@ -1,6 +1,17 @@
-from fastapi import APIRouter
+from typing import List, TYPE_CHECKING
+
+from fastapi import APIRouter, Depends
 
 from app.config import settings
+from app.repositories import TemperatureRepository
+from app.routers.dependencies import get_temp_repository, validate_day, get_system_city
+from app.schemas.temperature import TemperatureRead, CityRequest
+from app.services.weather import WeatherService
+
+
+if TYPE_CHECKING:
+    from app.models import City
+
 
 router = APIRouter(
     prefix=settings.api_prefix.temperatures,
@@ -10,7 +21,26 @@ router = APIRouter(
 
 @router.get(
     "/",
-    summary="",
+    summary="Get temperatures by date",
+    response_model=List[TemperatureRead],
 )
-async def get_temperatures() -> None:
-    return None
+async def get_temperatures(
+    day: int = Depends(validate_day),
+    city: "City" = Depends(get_system_city),
+    temp_repo: TemperatureRepository = Depends(get_temp_repository),
+) -> List[TemperatureRead]:
+    result = await temp_repo.get_all({"temperature_date": day, "city_id": city.id})
+    return [TemperatureRead.model_validate(elem) for elem in result]
+
+
+@router.post(
+    "/",
+    summary="Loading the temperature for the current time",
+    response_model=TemperatureRead,
+)
+async def load_temperatures(
+    params: CityRequest,
+) -> TemperatureRead:
+    service = WeatherService(params.city_name)
+    temp = await service.fetch_and_save()
+    return TemperatureRead.model_validate(temp)
