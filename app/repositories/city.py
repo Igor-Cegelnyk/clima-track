63f18e4ee2177e938e5fe745e48f63e21fc7341a
Ihor <il.cegelnik@gmail.com>
2025-10-01 15:36:32 +0300

    Added get_by_name to CityRepository

--- app/repositories/city.py
+++ app/repositories/city.py
@@ -1,6 +1,15 @@
+from typing import Annotated
+
+from sqlalchemy import select
+
 from app.models import City
 from app.repositories import SqlAlchemyRepository
 
 
 class CityRepository(SqlAlchemyRepository):
     model = City
+
+    async def get_by_name(self, city_name: str) -> Annotated[City, None]:
+        stmt = select(self.model).filter_by(name=city_name)
+        result = await self.session.scalar(stmt)
+        return result
