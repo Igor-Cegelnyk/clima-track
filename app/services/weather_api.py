b0f43a3d59fdf27e168fd85e930e787276760780
Ihor <il.cegelnik@gmail.com>
2025-10-01 16:49:20 +0300

    Fixed weather_api service

--- app/services/weather_api.py
+++ app/services/weather_api.py
@@ -1,19 +1,21 @@
 import asyncio
+from datetime import datetime
 
 import httpx
 
 from app.config import settings
+from app.models import City
 
 
 class WeatherAPI:
 
     @staticmethod
-    async def get_weather_info():
+    async def get_weather_info(city_name: str):
         async with httpx.AsyncClient() as client:
             resp = await client.get(
                 settings.weather_api.url,
                 params={
-                    "q": settings.weather_api.city,
+                    "q": city_name,
                     "appid": settings.weather_api.api_key,
                     "units": "metric",
                 },
@@ -21,9 +23,15 @@ class WeatherAPI:
             resp.raise_for_status()
             return resp.json()
 
-    async def get_temperature(self):
-        weather = await self.get_weather_info()
-        return {"temperature": weather["main"]["temp"], "date": weather["dt"]}
+    async def get_temperature(self, city: City):
+        weather = await self.get_weather_info(city_name=city.name)
+        dt = datetime.fromtimestamp(weather["dt"]).astimezone()
+        return {
+            "city_id": city.id,
+            "temperature": weather["main"]["temp"],
+            "temperature_date": int(dt.strftime("%Y%m%d")),
+            "temperature_time": int(dt.strftime("%H%M%S")),
+        }
 
 
 if __name__ == "__main__":
